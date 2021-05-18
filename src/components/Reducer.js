65b3706fbcb9788bdfbebb5a0d99feb64d41a1c7
NazarShulak <shulakna@gmail.com>
2021-05-18 18:12:28 +0300

    made reducer

---
+++ src/components/Reducer.js
@@ -0,0 +1,42 @@
+import {createStore} from "redux";
+
+const initialState = {
+    counter: 0
+};
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'INC':
+            return {
+                ...state,
+                counter: state.counter + 1
+            }
+        case 'INC_100':
+            return {
+                ...state,
+                counter: state.counter + 100
+            }
+        case 'DEC':
+            return {
+                ...state,
+                counter: state.counter - 1
+            }
+        case 'DEC_100':
+            return {
+                ...state,
+                counter: state.counter - 100
+            }
+        case 'RESET':
+            return {
+                ...state,
+                counter: 0
+            }
+        default:
+            return state
+    }
+
+}
+
+
+const store = createStore(reducer);
+export default store;
