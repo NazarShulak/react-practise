dbcffa8dc7ce4d76d1bb774751a0f65e028b52f7
NazarShulak <shulakna@gmail.com>
2021-05-18 18:11:31 +0300

    added routes and links

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import {BrowserRouter as Router, Switch, Link, Route} from "react-router-dom";
-import Inventory from "./components/inventory/Inventory";
+import Counter from "./components/Counter";
+import {Provider} from "react-redux";
+import store from "./components/Reducer";
+
 
 function App() {
     return (
-        <Router>
-            <div>
-                <br/>
-                <Link to={"/inventory"}>Show all inventory</Link>
-                <hr/>
-                <Switch>
-
-                    <Route exact={true} path={"/inventory"}
-                           render={(props) => {
-                        return <Inventory item={props}/>
-                    }}/>
-                </Switch>
-            </div>
-        </Router>
-    );
+        <div>
+            <Provider store={store}>
+                <Router>
+                    <Link to={"/"}> home page</Link>
+                    <br/>
+                    <Link to={"/counter"}> go to counter</Link>
+                    <hr/>
+                    <Switch>
+                        <Route exact path={"/"} render={(props) => <div>HOME SWEET HOME!!!</div>}/>
+                        <Route exact path={"/counter"} component={Counter}/>
+                    </Switch>
+                </Router>
+            </Provider>
+        </div>
+    )
+        ;
 }
 
 export default App;
